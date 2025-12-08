f904feea4034a55ae5574b7e6853ec85e5604f16
lorem10 <momenei.alireza1382.15@gmail.com>
2025-12-08 05:48:21 +0330

    adjust theme toggle logic and component

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { isDark, toggleDarkWithTransition } from '../logic'
+
+function handleToggle(event: MouseEvent) {
+  toggleDarkWithTransition(event)
+}
+</script>
+
+<template>
+  <button
+    class="select-none rounded-full p-sp-1 outline-none"
+    title="Toggle Color Scheme"
+    :aria-pressed="isDark"
+    @click="handleToggle"
+  >
+    <div class="i-ri-sun-line dark:i-ri-moon-line text-xl" />
+  </button>
+</template>
